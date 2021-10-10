1c58205653b0abb1c57702e96db1a7db22ae1dc0
renantescaro <renantescaro@yahoo.com.br>
2021-10-10 16:01:05 -0300

    classe responsavel pela comunicação com o banco de dados

---
+++ flaskr/dao/banco.py
@@ -0,0 +1,23 @@
+from mysql import connector
+from flaskr.utils.config import Config
+
+
+class Banco:
+    def __init__(self):
+        self.mydb    = connector.connect(
+            host     = Config.get('host'),
+            user     = Config.get('user'),
+            password = Config.get('password'),
+            database = Config.get('database') )
+
+
+    def selecionar(self, query, parametros=None):
+        my_cursor = self.mydb.cursor()
+        my_cursor.execute(query, parametros)
+        return my_cursor.fetchall()
+
+
+    def executar(self, query, parametros=None):
+        my_cursor = self.mydb.cursor()
+        my_cursor.execute(query, parametros)
+        self.mydb.commit()
